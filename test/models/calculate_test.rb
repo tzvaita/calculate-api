36e106918f51807710b4638c156e195011b07e0f
tzvaita <tzvaita@gmail.com>
2021-12-31 14:17:54 +0200

    Add model tests

--- test/models/calculate_test.rb
+++ test/models/calculate_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class CalculateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @calculate = Calculate.new(a: 4, b: 6, operation: "times")
+  end
+
+  test "should be valid" do
+    assert @calculate.valid?
+  end
+
+  test "a should be present" do
+    @calculate.a = ""
+    assert_not @calculate.valid?
+  end
+
+  test "b should be present" do
+    @calculate.b = ""
+    assert_not @calculate.valid?
+  end
+
+  test "operation should be present" do
+    @calculate.operation = ""
+    assert_not @calculate.valid?
+  end
 end
