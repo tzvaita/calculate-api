45f261048322ef4f466165a2ce193e8d598d1611
tzvaita <tzvaita@gmail.com>
2021-12-31 14:16:23 +0200

    Add guard and minitest reporters

--- test/controllers/calculates_controller_test.rb
+++ test/controllers/calculates_controller_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class CalculatesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should post to calculate" do
+    post calculate_url
+    assert_response :success
+  end
 end
